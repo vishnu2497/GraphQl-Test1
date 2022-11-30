6b9111b56ffdfd228697dd88a6624820e3cefbff
Sowmiya <sowmiyaradha1999@gmail.com>
2022-11-30 07:42:58 +0530

    abase project with working code

---
+++ src/main/java/com/vishnu/graphql/GqlDemo/GqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vishnu.graphql.GqlDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GqlDemoApplication.class, args);
+	}
+
+}
